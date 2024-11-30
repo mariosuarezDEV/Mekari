1a7a03e8d9ed4717a4a6e168b6450f3a2d637538
Luis Mario Suarez <lmcervantessuarez@gmail.com>
2024-11-30 13:56:42 -0600

    Eliminar archivos y configuraciones no utilizados, actualizar dependencias y mejorar la estructura del proyecto con nuevos componentes y estilos

--- Mekari/vite.config.ts
+++ Mekari/vite.config.ts
@@ -1,16 +1,7 @@
-import path from "path"
 import { defineConfig } from 'vite'
-import react from "@vitejs/plugin-react"
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [vue()],
 })
-
-
