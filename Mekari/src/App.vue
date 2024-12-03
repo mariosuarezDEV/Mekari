bd6e31b19fcfc60bd771dbb8978165b27fed8409
Luis Mario Suarez <lmcervantessuarez@gmail.com>
2024-12-03 16:00:48 -0600

    Agregar barra de navegación en App.vue, crear vistas de Dashboard, Login y RRHH, y establecer métodos de saludo

--- Mekari/src/App.vue
+++ Mekari/src/App.vue
@@ -1,10 +1,28 @@
 <script lang="ts" setup>
-const title = 'Welcome to Mekari'
+import { RouterLink, RouterView  } from 'vue-router'
 
 </script>
 
 <template>
   <div>
-    <h1>{{ title }}</h1>
+    <!-- Barra de Navegación -->
+     <div className="grid grid-cols-2 bg-gray-700 text-white shadow-lg">
+
+      <div>
+        <h1 className="font-bold text-2xl p-4">
+          Mekari
+        </h1>
+      </div>
+
+      <div className="flex flex-nowrap gap-10 justify-end items-center mr-10">
+        <RouterLink to="/" className="">Dashboard</RouterLink>
+        <RouterLink to="/perfil" className="">Mi perfil</RouterLink>
+        <RouterLink to="/configuracion" className="">Configuración</RouterLink>
+        <RouterLink to="/logout" className="">Cerrar Sesión</RouterLink>
+      </div>
+
+     </div>
+    <RouterView />
   </div>
+
 </template>
---
+++ Mekari/src/views/DashboardView.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    <h1 className="font-bold text-3xl">
+      Esta es la vista de Dashboard
+    </h1>
+  </div>
+</template>
